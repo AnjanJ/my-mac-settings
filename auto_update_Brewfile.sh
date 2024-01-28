a672baca290562a9d92e6c8cc09a54190bc0de16
Anjan Jagirdar <anjan.jagirdar@gmail.com>
2024-01-28 11:55:58 +0530

    Add echo statements

--- auto_update_Brewfile.sh
+++ auto_update_Brewfile.sh
@@ -7,12 +7,15 @@ BREWFILE_DIR=~/
 SETTINGS_REPO_DIR=~/code/my-mac-settings
 
 # Update Homebrew
+echo "Update Brew Formuales"
 brew update
 
 # Upgrade installed formulae
+echo "Update apps installed via Homebrew"
 brew upgrade
 
 # Upgrade apps installed via mac app store
+echo "Update apps installed via Mac App"
 mas upgrade # Learn more here -> https://github.com/mas-cli/mas
 
 # Backup existing Brewfile
